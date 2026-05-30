c5291db734c093e95328a40abc4707c256556df9
ZCL-Creater <18277191101@163.com>
2026-05-30 15:44:06 +0800

    fix: 硬编码base路径修复线上资源加载失败

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/shanhaijng/' : '/',
+  base: '/shanhaijng/',
   server: {
     host: true,
     port: 3000,
